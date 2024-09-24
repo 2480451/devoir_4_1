e356e8435d4b510b4e9c77af5888db625f908003
ED <2480451@cstjean.qc.ca>
2024-09-24 10:14:57 -0400

    V4

--- main.py
+++ main.py
@@ -20,32 +20,36 @@ rabais_achat_en_petit_sac: float  # Montant soustrait au prix de base de l'achat
 rabais_achat_en_grand_sac: float  # Montant soustrait au prix de base de l'achat en grands sacs
 prix_base_en_grand_sac: float  # Prix sans rabais pour l'achat en grands sacs
 prix_achat_en_grand_sac: float  # Prix final si achat en grands sacs
-quantité_petit_sac_nécessaire: float
-quantité_grand_sac_nécessaire: float
-meilleur_prix_achat: float
-meilleure_option_achat: str
-quantité_réelle_en_petits_sacs: float
-quantité_réelle_en_grands_sacs: float
-économie_petits_sacs: float
-économie_grands_sacs: float
+quantité_petit_sac_nécessaire: float  # Quantité de petits sacs nécessaire pour atteindre la quantité désirée
+quantité_grand_sac_nécessaire: float  # Quantité de grands sacs nécessaire pour atteindre la quantité désirée
+meilleur_prix_achat: float  # Prix d'achat le plus bas des trois options d'achat
+meilleure_option_achat: str  # Nom du type d'achat ayant le prix d'achat le plus faible
+quantité_réelle_en_petits_sacs: float  # Quantité du produit achetée si achat en petits sacs
+quantité_réelle_en_grands_sacs: float  #
+économie_petits_sacs: float  # Économie grâce à l'achat en petits sacs
+économie_grands_sacs: float  # Économie grâce à l'achat en grands sacs
 
 # LOGIQUE
 quantité_désirée = float(input("Veuillez saisir la masse désirée du produit (en kg): "))
 prix_base_par_kg = float(input("Veuillez saisir le prix de base par kg du produit: "))
-
 prix_achat_vrac = quantité_désirée * prix_base_par_kg
+# Quantité sacs nécessaire
 quantité_petit_sac_nécessaire = ceil(quantité_désirée / QUANTITÉ_PETIT_SAC)
 quantité_grand_sac_nécessaire = ceil(quantité_désirée / QUANTITÉ_GRAND_SAC)
+# Prix sans rabais
 prix_base_en_petit_sac = (quantité_petit_sac_nécessaire * QUANTITÉ_PETIT_SAC) * prix_base_par_kg
 prix_base_en_grand_sac = (quantité_grand_sac_nécessaire * QUANTITÉ_GRAND_SAC) * prix_base_par_kg
+# Rabais
 rabais_achat_en_petit_sac = TAUX_RABAIS_PETIT_SAC * prix_base_en_petit_sac
 rabais_achat_en_grand_sac = TAUX_RABAIS_GRAND_SAC * prix_base_en_grand_sac
 prix_achat_en_petit_sac = prix_base_en_petit_sac - rabais_achat_en_petit_sac
 prix_achat_en_grand_sac = prix_base_en_grand_sac - rabais_achat_en_grand_sac
+# Quantité réelle
 quantité_réelle_en_petits_sacs = quantité_petit_sac_nécessaire * QUANTITÉ_PETIT_SAC
 quantité_réelle_en_grands_sacs = quantité_grand_sac_nécessaire * QUANTITÉ_GRAND_SAC
+# Économie
 économie_petits_sacs = prix_achat_vrac - prix_achat_en_petit_sac
-économie_grands_sacs = prix_achat_en_grand_sac - prix_achat_en_grand_sac
+économie_grands_sacs = prix_achat_vrac - prix_achat_en_grand_sac
 
 print("")
 print(f"Le prix d'achat du produit en vrac est de: {prix_achat_vrac:17.2f} $")
@@ -53,6 +57,7 @@ print(f"Le prix d'achat du produit en petits sacs est de: {prix_achat_en_petit_s
 print(f"Le prix d'achat du produit en grands sacs est de: {prix_achat_en_grand_sac:10.2f} $")
 print("")
 
+#  Pour afficher la meilleure option
 if prix_achat_en_petit_sac < prix_achat_vrac and prix_achat_en_petit_sac < prix_achat_en_grand_sac:
     meilleure_option_achat = "petits sacs"
     print(f"La meilleure option est l'achat en {meilleure_option_achat}.")
@@ -63,6 +68,7 @@ else:
     meilleure_option_achat = "vrac"
     print(f"La meilleur option est l'achat en {meilleure_option_achat}.")
 
+#  Pour afficher la quantité réelle achetée et l'économie réalisée
 if meilleure_option_achat == "petits sacs":
     print(f"La quantité réelle achetée sera de {quantité_réelle_en_petits_sacs} kg.")
     print(f"L'économie sera de {économie_petits_sacs:.2f} $ par rapport à l'achat en vrac.")
